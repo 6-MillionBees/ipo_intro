fb038eb0e62ecd15d018f6802fc53ba787565c7e
6MillionBees <BoettcherAr321@northwested.org>
2024-09-18 09:17:32 -0400

    Add files via upload

---
+++ ipo1.py
@@ -0,0 +1,13 @@
+# Arden Boettcher
+# 9/18/24
+# IPO Intro
+
+num1 = int(input('Enter a number: (Example: 7)\n'))
+num2 = int(input('Enter another number: (example: 4)\n'))
+
+total = num1 + num2
+
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print()
+print(f'the sum of {num1} and {num2} is: {total}')
